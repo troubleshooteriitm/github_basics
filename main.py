1581a1e602410b1ad4f65d5189c397c1eb56be80
unknown <magizhtechnologies24@gmail.com>
2026-06-05 12:27:21 +0530

    flask

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("hello world")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello, World!"
+
+@app.route("/abc")
+def abc():
+    return "This is the ABC route."
+
+
+@app.route("/<name>")
+def greet(name):
+    return f"Hello, {name}!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+    
